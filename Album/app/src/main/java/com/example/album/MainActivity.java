2969949ae5e5544f9e4cebe38384aae23c2249de
rui xu <rachelxu19911005@gmail.com>
2020-10-03 10:53:01 -0400

    finish album

--- Album/app/src/main/java/com/example/album/MainActivity.java
+++ Album/app/src/main/java/com/example/album/MainActivity.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageView mImage;
+    private TextView mText;
+    private int count=0;
+    private int total;
+    private String[] title;
+    private int[] images;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,17 +22,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initData();//(2)
     }
     private void initData(){
-        String[] title={"first pic","second pic","third pic","forth pic"};
-        int[] images={};
+        title=new String[]{"first pic","second pic","third pic","forth pic"};
+        images=new int[]{R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d};
+        total=title.length;
+        mImage.setImageResource(images[count]);
+        mText.setText(title[count]);
     }
     private void initView(){
         mImage=findViewById(R.id.iv_show);
+        mText=findViewById(R.id.tv_show);
         findViewById(R.id.btn_previous).setOnClickListener(this);
         findViewById(R.id.btn_next).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-
+        if(view.getId()==R.id.btn_previous){
+            if(count==0){
+                count=total-1;
+            }else{
+                count--;
+            }
+        }else if(view.getId()==R.id.btn_next){
+            if(count==total-1){
+                count=0;
+            }else{
+                count++;
+            }
+        }
+        updateImageAndTitle();
+    }
+    private void updateImageAndTitle(){
+        mImage.setImageResource(images[count]);
+        mText.setText(title[count]);
     }
 }
